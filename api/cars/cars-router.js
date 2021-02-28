69e39c693daddcacb187aa0b010e8ce8d1db46c7
Clare Jackson <clare-jackson@lambdastudents.com>
2021-02-28 14:58:37 -0500

    router done

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,11 +1,33 @@
 // DO YOUR MAGIC
-
-
+const router = require('express').Router()
+const cars = require('./cars-model')
+const { checkCarId, checkCarPayload, checkVinNumberUnique, checkVinNumberValid } = require('./cars-middleware')
 
 // [GET] /api/cars returns an array of cars sorted by id (or an empty array if there aren't any).
-
+router.get('/', async (req, res, next) => {
+  try {
+    const values = await cars.getAll()
+    res.status(200).json(values)
+  } catch (err) {
+    next(err)
+  }
+})
 
 // [GET] /api/cars/:id returns a car by the given id.
-
+router.get('/:id', checkCarId, async (req, res, next) => {
+  try {
+    res.status(200).json(req.car)
+  } catch (err) {
+    next(err)
+  }
+})
 
 // [POST] /api/cars returns the created car. Leading or trailing whitespace on budget name should be trimmed before saving to db.
+router.post('/', checkCarPayload, checkVinNumberUnique, checkVinNumberValid, async (req, res, next) => {
+  try {
+    const newCar = cars.create(res.car)
+    res.status(201).json(newCar)
+  } catch (err) {
+    next(err)
+  }
+})
--- api/server.js
+++ api/server.js
@@ -4,10 +4,10 @@ const server = express()
 
 // DO YOUR MAGIC
 
-// const carsRouter = require("./cars/cars-router")
+const carsRouter = require("./cars/cars-router")
 
 server.use(express.json())
-// server.use("/api/cars", carsRouter)
+server.use("/api/cars", carsRouter)
 
 server.use((err, req, res, next) => {
 	console.log(err)
